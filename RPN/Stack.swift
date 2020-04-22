9a045a22c1f0416df62647b8ca0a15fe94ea1ab8
Christopher DeVito <christopher.devito@protonmail.com>
2020-04-22 12:46:34 -0400

    Added documentation.

--- RPN/Stack.swift
+++ RPN/Stack.swift
@@ -8,23 +8,31 @@
 
 import Foundation
 
+
+/// A simple data type for representing elements as a "stack" of things
 struct Stack<Element>: ExpressibleByArrayLiteral {
     
     var elements: [Element]
     typealias ArrayLiteralElement = Element
     
-    internal init(elements: [Element]) {
+    init(arrayLiteral elements: Element...) {
         self.elements = elements
     }
     
+    /// Add things to the top of the stack.
+    /// - Parameter element: The element to be added to the top of the stack.
     mutating func push(_ element: Element) {
         
     }
     
+    /// Remove the most recent item from the stack.
+    /// - Returns: The element that was removed, or `nil` if there were no elements left.
     mutating func pop() -> Element? {
         
     }
     
+    /// Take a look at what the most recent element added to the stack was.
+    /// - Returns: The most recent element added to the stack.
     func peek() -> Element? {
         
     }
