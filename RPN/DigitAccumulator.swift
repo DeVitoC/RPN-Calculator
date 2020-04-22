07239e3e8d56e5c6ff4c362172adaa9f2e75b199
Christopher DeVito <christopher.devito@protonmail.com>
2020-04-22 13:03:20 -0400

    Added basic API to the DigitAccumulator.

--- RPN/DigitAccumulator.swift
+++ RPN/DigitAccumulator.swift
@@ -8,3 +8,35 @@
 
 import Foundation
 
+/// The error a digit accumulator may throw if an invalid digit is supplied.
+enum DigitAccumulatorError: Error {
+    case invalidNumberValue
+    case extraDecimalPoint
+}
+
+/// Collects and accumulates digits into a number.
+struct DigitAccumulator {
+    
+    enum Digit: Equatable {
+        case decimalPoint
+        case number(Int)
+    }
+    
+    /// Add a new digit to the accumulator. Throws an error if the digit is invalid.
+    /// - Parameter digit: The digit to add.
+    /// - Throws: `DigitAccumulatorError`
+    mutating func add(digit: Digit) throws {
+        
+    }
+    
+    /// The current value in the Digit Accumulator, or `nil` if it can't be interpreted as a `Double`.
+    var value: Double? {
+        
+    }
+    
+    /// Reset the accumulator.
+    mutating func clear() {
+        
+    }
+    
+}
