1f74abcbbb2f1e42b24e09460dbf36e72f72335e
Christopher DeVito <christopher.devito@protonmail.com>
2020-04-22 12:33:55 -0400

    Added basic array methods.

--- RPN/Stack.swift
+++ RPN/Stack.swift
@@ -8,4 +8,24 @@
 
 import Foundation
 
-
+struct Stack<Element>: ExpressibleByArrayLiteral {
+    
+    var elements: [Element]
+    typealias ArrayLiteralElement = Element
+    
+    internal init(elements: [Element]) {
+        self.elements = elements
+    }
+    
+    mutating func push(_ element: Element) {
+        
+    }
+    
+    mutating func pop() -> Element? {
+        
+    }
+    
+    func peek() -> Element? {
+        
+    }
+}
