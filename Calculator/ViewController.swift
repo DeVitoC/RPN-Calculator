3665a0930cf3ddb185fe175b24561e835509aa36
Christopher DeVito <christopher.devito@protonmail.com>
2020-04-22 12:09:44 -0400

    Initial commit. Created calculator project.

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Christopher Devito on 4/22/20.
+//  Copyright © 2020 Christopher Devito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
