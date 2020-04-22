c95ca2c09cbd27feb14f2207909b60fb3fd29e5a
Christopher DeVito <christopher.devito@protonmail.com>
2020-04-22 13:39:15 -0400

    Added implementation for the Calculator.

--- RPN/Calculator.swift
+++ RPN/Calculator.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+public struct Calculator {
+    
+    public enum Operator {
+        case add
+        case subtract
+        case multiply
+        case divide
+    }
+    
+    private var stack: Stack<Double> = [0, 0]
+    
+    public init() { }
+    
+    public mutating func push(_ number: Double) {
+        stack.push(number)
+    }
+    
+    public mutating func push(operator: Operator) {
+        let operand1 = stack.pop() ?? 0.0
+        let operand2 = stack.pop() ?? 0.0
+        
+        let results: Double
+        
+        switch `operator` {
+        case .add:
+            results = operand1 + operand2
+        case .subtract:
+            results = operand1 - operand2
+        case .multiply:
+            results = operand1 * operand2
+        case .divide:
+            results = operand1 / operand2
+        }
+        
+        stack.push(results)
+    }
+    
+    public var topValue: Double? {
+        return stack.peek()
+    }
+    
+    public mutating func clearStack() {
+        stack = []
+    }
+}
